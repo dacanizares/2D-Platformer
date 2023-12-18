9799d37ae521f75db5aebf8f03707d02edfb0c57
dacanizares <dacanizares@outlook.com>
2023-12-18 18:56:26 -0500

    Improved code

--- main.py
+++ main.py
@@ -13,25 +13,24 @@ from tilemap_scripts import load_map
 game_sdl.start(DISP_W, DISP_H)
 spr_idle, spr_walk, spr_jump = load_sprites()
 
+# Characters
 player = Character(40, 40, pygame.Rect(0,0,20,25), spr_idle, spr_walk, spr_jump, CharacterBehaviors.PLAYER)
 jumping_ai = Character(60, 40, pygame.Rect(0,0,20,25), spr_idle, spr_walk, spr_jump, CharacterBehaviors.JUMPING_AI)
 basic_ai = Character(100, 40, pygame.Rect(0,0,20,25), spr_idle, spr_walk, spr_jump, CharacterBehaviors.BASIC_AI)
+characters = [player, jumping_ai, basic_ai]
 
+# Map
 tilemap = load_map('maps/map1.json')
 camera = Camera(0, 0, always_centered=False)
 
-characters = [player, jumping_ai, basic_ai]
-
-clock = game_sdl.clock()
-
+# Music
 pygame.mixer.init()
 pygame.mixer.music.load("sound/hyperfun.mp3")
 pygame.mixer.music.play(100)
 
-sheet = game_sdl.load_image('graphics/blocks1.png')
-
 # Gameloop
 start_characters(characters, character_behaviors)
+clock = game_sdl.clock()
 while True:    
     events = game_sdl.get_events()
     if 'QUIT' in events:
