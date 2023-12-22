c3ee595bad8b879e75ba6cbe6ab1d236e97a10a9
dacanizares <dacanizares@outlook.com>
2023-12-21 23:58:13 -0500

    Cleaned code

--- game_scripts.py
+++ game_scripts.py
@@ -1,8 +1,7 @@
-
-
 import pygame
-from constants import DISP_H, DISP_W, VY_COLLIDE
-from game_structs import Camera, Character
+from game_structs import *
+from constants import *
+from collision_scripts import *
 from tilemap_structs import Tilemap
 
 
@@ -29,22 +28,6 @@ def start_characters(characters: list[Character], behaviors: dict):
     for character in characters:
         behaviors[character.behavior_type].on_start(character)
 
-
-# Project collider to tilemap.
-# col: pygame.Rect
-# tileset_idx: where to get dimensions from, by default idx=0
-#
-# Returns left, right, top and bot rows or cols limiting with.
-# The collider center point is at middle bottom.
-def _project_collider_to_tilemap(col: pygame.Rect, tilemap: Tilemap, tileset_idx: int=0):
-    tile_w = tilemap.tilesets[tileset_idx].tilew
-    tile_h = tilemap.tilesets[tileset_idx].tileh
-    left = (col.x - col.w / 2 + 1) / tile_w
-    right = (col.x + col.w / 2 - 1) / tile_w
-    top = (col.y - col.h + 1) / tile_h
-    bot = (col.y - 1) / tile_h
-    return left, right, top, bot
-
 # Updates entities and colliding events
 # events: pygame events to send to entities
 def update_characters(characters: list[Character], behaviors: dict, events: dict, tilemap: Tilemap):
@@ -57,11 +40,11 @@ def update_characters(characters: list[Character], behaviors: dict, events: dict
         col = character.collider
 
         # UPDATE X --------------------------------------
-        left, right, top, bot = _project_collider_to_tilemap(col, tilemap)
+        left, right, top, bot = project_collider_to_tilemap(col, tilemap)
 
         # Search for limits
-        min_x = _search_collisions(tilemap, left, top, bot, -1 , 0)
-        max_x = _search_collisions(tilemap, right, top, bot,  1 , 0) 
+        min_x = search_collisions(tilemap, left, top, bot, -1 , 0)
+        max_x = search_collisions(tilemap, right, top, bot,  1 , 0) 
 
         # Limit X
         limit = min_x * tilemap.tilew + tilemap.tilew + col.w / 2
@@ -76,11 +59,11 @@ def update_characters(characters: list[Character], behaviors: dict, events: dict
         col.x = character.x
 
         # UPDATE Y ------------------------------------
-        left, right, top, bot = _project_collider_to_tilemap(col, tilemap)
+        left, right, top, bot = project_collider_to_tilemap(col, tilemap)
 
         # Search for limits
-        min_y = _search_collisions(tilemap, top, left, right, 0, -1)
-        max_y = _search_collisions(tilemap, bot, left, right, 0, 1)
+        min_y = search_collisions(tilemap, top, left, right, 0, -1)
+        max_y = search_collisions(tilemap, bot, left, right, 0, 1)
 
         # Limit Y
         limit = min_y * tilemap.tileh + tilemap.tileh + col.h
@@ -114,63 +97,3 @@ def update_characters(characters: list[Character], behaviors: dict, events: dict
                 behavior_b = behaviors[character_b.behavior_type]
                 behavior_a.on_collide(character_a, is_top_a)
                 behavior_b.on_collide(character_b, not is_top_a)
-
-
-# Search for static objects to collide with
-# start: starting point
-# a: lower bound
-# b: upper bound (a < b)
-# dx, dy: moving direction
-#
-# Returns: None if no colliders found. Otherwise a value with the ROW or COLUMN of the collider.
-#
-# Example: 
-#  to trace from x = 1, to the left, from tiles y = 2 to y = 3
-#  search(1, 2, 5, 1, 0)
-#
-# Result:
-#    0 1 2 3 4 5 ... MAX_DIST_COL
-#  0 . . . . . .
-#  1 . . . . . .
-#  2 . X X X X X 
-#  3 . X X X X X 
-#  4 . . . . . . 
-#
-#  It will search on the position marked with X
-
-def _search_collisions(tilemap: Tilemap, start, a, b, dx, dy):
-    # Grant that these vars are int
-    start = int(start)
-    a = int(a)
-    b = int(b)
-
-    # Validate that we arent searching on diagonals
-    if dx != 0 and dy != 0:
-        raise Exception('collision search', 'unable to lock on diagonals.')
-
-    if a < 0:
-        return -1
-
-    # Set limits, by default 0, width or height according to dx and dy
-    limit = -1
-    if dx > 0:
-        limit = tilemap.current_width
-    elif dy > 0:
-        limit = tilemap.current_height
-            
-    # Search for static objects!
-    while True:
-        # If we reached the limit
-        if start == limit:
-            return start
-
-        # Scan!
-        for i in range(a, b + 1):
-            # x changing
-            if dx != 0 and tilemap.current_map[i][start] not in tilemap.no_collision:                    
-                return start
-            # y changing
-            elif dy != 0 and tilemap.current_map[start][i] not in tilemap.no_collision:
-                return start
-        # Advance!
-        start += (dx + dy)
